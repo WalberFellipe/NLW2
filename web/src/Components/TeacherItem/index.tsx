1f0f6904724b4406fa9262466a510755ea79248e
WalberFellipe <walberfellipe97@gmail.com>
2020-08-09 21:51:31 -0300

    Finished NLW Web Version 1.0

--- web/src/Components/TeacherItem/index.tsx
+++ web/src/Components/TeacherItem/index.tsx
@@ -1,42 +1,58 @@
 import React from "react";
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
-import './styles.css'
+import "./styles.css";
+import api from "../../Services/api";
 
-function TeacherItem() {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars2.githubusercontent.com/u/51340360?s=460&u=9edfb8c41900d8ff429b4daeb31f3fae8a397a18&v=4"
-          alt="Walber Fellipe"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Walber Fellipe</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br></br>
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das
-        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
-        uma das minhas explosões.
-      </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$150,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
-}
+};
 
 export default TeacherItem;
--- web/src/pages/TeachersList/index.tsx
+++ web/src/pages/TeachersList/index.tsx
@@ -1,39 +1,88 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
 import PageHeader from "../../Components/PageHeader";
-import TeacherItem from "../../Components/TeacherItem";
+import TeacherItem, { Teacher } from "../../Components/TeacherItem";
+import Input from "../../Components/Input";
+import Select from "../../Components/Select";
 
 import "./styles.css";
 
+import api from "../../Services/api";
+
 
 function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeekDay] = useState("");
+  const [time, setTime] = useState("");
+
+  async function searchTeachers(e: FormEvent) {
+    e.preventDefault();
+
+    const response = await api.get("classes", {
+        params: {
+          subject,
+          week_day,
+          time,
+        },
+      })
+      .then((response) => {
+        setTeachers(response.data);
+      });
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis. ">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matérias</label>
-            <input type="text" id="subject" />
-          </div>
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Materia"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "História", label: "História" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Português", label: "Português" },
+              { value: "Química", label: "Química" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            value={week_day}
+            onChange={(e) => setWeekDay(e.target.value)}
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quarta-feira" },
+              { value: "4", label: "Quinta-Feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            onChange={(e) => setTime(e.target.value)}
+          />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Matérias</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
+      {teachers.map((teacher: Teacher) => (
+					<TeacherItem key={teacher.id} teacher={teacher} />
+				))}
       </main>
     </div>
   );
